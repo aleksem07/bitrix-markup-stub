942b984938b1bd1541da361ac855fdd1a0fe9050
aleksem07 <aleksem07@gmail.com>
2024-07-12 00:04:26 +0300

    feat: add grid form and add two sliders

--- src/core/config/js/nouislider/price-range.js
+++ src/core/config/js/nouislider/price-range.js
@@ -2,14 +2,26 @@
   document.addEventListener("DOMContentLoaded", () => {
 
   const slider = document.getElementById('price-slider');
-  
+  const marginMin = document.getElementById('price-slider-value-min');
+  const marginMax = document.getElementById('price-slider-value-max');
+
   noUiSlider.create(slider, {
     start: [0, 700],
     connect: true,
+    tooltips: [
+      false,
+      { to: value => `${Math.round(value)} $`, from: value => value },
+    ],
     range: {
       min: 0,
       max: 999,
     },
+  });
+
+  slider.noUiSlider.on('update', function (values, handle) {
+    const value = Math.round(values[handle]);
+
+    handle ? marginMax.innerHTML = `${value} $` : marginMin.innerHTML = `${value} $`;
   });
 
 console.log(slider);
--- src/core/config/js/nouislider/thc.js
+++ src/core/config/js/nouislider/thc.js
@@ -1,1 +1,20 @@
-console.log('thc')
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.getElementById('thc');
+  const max = document.getElementById('thc-value');
+
+  noUiSlider.create(slider, {
+    start: 65,
+    connect: 'lower',
+    tooltips: { to: value => `${Math.round(value)} $`, from: value => value },
+    range: {
+      min: 0,
+      max: 100,
+    },
+  });
+
+  slider.noUiSlider.on('update', function (values, handle) {
+    const value = Math.round(values[handle]);
+      max.innerHTML = `${value} $`
+  });
+
+});
