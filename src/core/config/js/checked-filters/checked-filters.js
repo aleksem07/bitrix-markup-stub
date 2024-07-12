5f6b93e485cd8d12873bc7e84123289eac8ad12d
aleksem07 <aleksem07@gmail.com>
2024-07-12 18:10:57 +0300

    feat: add checked filter opacity

--- src/core/config/js/checked-filters/checked-filters.js
+++ src/core/config/js/checked-filters/checked-filters.js
@@ -9,23 +9,27 @@
           .replace(/>/g, '&gt;');
 
         if (checkbox.checked) {
-          const li = document.createElement("li");
+          const li = document.createElement('li');
           const p = document.createElement('p');
-          const button = document.createElement("button");
+          const button = document.createElement('button');
 
-          li.setAttribute("data-value", idx);
+          li.setAttribute('data-value', idx);
           li.classList.add('checked_item');
           button.classList.add('checked_button');
           p.innerHTML = itemValue;
 
           li.append(p, button);
           checkedListBox.appendChild(li);
-          button.onclick = (e) => {
+
+          requestAnimationFrame(() => {
+            li.style.opacity = 1;
+          });
+
+          button.onclick = e => {
             e.stopPropagation();
             checkbox.checked = false;
             li.remove();
-          }
-
+          };
         } else {
           const listItem = checkedListBox.querySelector(
             `li[data-value="${idx}"]`
