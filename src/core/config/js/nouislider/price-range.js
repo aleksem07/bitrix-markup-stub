d0c4a636fe19004e9803da9e4c0e2a93e6e9423d
aleksem07 <aleksem07@gmail.com>
2024-07-15 13:36:58 +0300

    fix: add func get tooltip value

--- src/core/config/js/nouislider/price-range.js
+++ src/core/config/js/nouislider/price-range.js
@@ -1,11 +1,14 @@
 // =require add-slider.js
+
+const getTooltipValue = {
+  to: value => `${Math.round(value)} $`,
+  from: value => value,
+};
+
 addSlider('price-slider', 'price-slider-value-min', 'price-slider-value-max', {
   start: [0, 600],
   connect: true,
-  tooltips: [
-    { to: value => `${Math.round(value)} $`, from: value => value },
-    { to: value => `${Math.round(value)} $`, from: value => value },
-  ],
+  tooltips: [getTooltipValue, getTooltipValue],
   range: {
     min: 0,
     max: 999,
